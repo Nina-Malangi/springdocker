92e0f872d3f6b4ba656d44dbd36da753e64e23fe
Naveen MS <naveenms@Naveens-MacBook-Pro.local>
2021-08-06 22:51:23 +0400

    Adding Initial File to Git

---
+++ src/main/java/com/nina/springdocker/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.nina.springdocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/msg")
+    public String getHelloWorldMessage() {
+        return "Hello Welcome to Spring Boot + Docker";
+    }
+}
